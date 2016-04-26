7a972af89ff48086e8ddda93e907c7f59ff3e425
Joel Ward <jmward@gmail.com>
2016-04-25 21:25:20 -0700

    fix typos

--- bkeys.js
+++ bkeys.js
@@ -1,18 +1,18 @@
 print=console.log.bind(console);
 print("bkeys.js");
-callback=print;
-function StrCode(e){
-    return String.fromCharCode(e.keyCode).toLowerCase();
-}
+callback=function(x){
+    var li=document.createElement("li");
+    li.innerHTML=x;
+    document.body.appendChild(li);}
+function StrCode(e){ return String.fromCharCode(e.keyCode).toLowerCase(); }
 function onkeydown(e){
     var kc=e.keyCode;
-    var ret;
     switch(kc){
     case 9:
-	callback( e.shiftKey?"<backtab>":"<tab>" );
+	callback( e.shiftKey?"S-TAB":"TAB" );
 	return false;
     case 8:
-	callback( e.altKey||e,metaKey?"M-DEL":"DEL" );
+	callback( e.altKey||e.metaKey?"M-DEL":"DEL" );
 	return false;
     case 27:
 	callback("ESC");
@@ -25,11 +25,11 @@ function onkeydown(e){
 	return;
     }
     if(e.ctrlKey||e.altKey||e.metaKey){
+	var ret;
 	switch(kc){
 	case 13: ret = "RET"; break;
 	case 32: ret = "SPC"; break;
-	default: ret = StrCode(e);
-	}
+	default: ret = StrCode(e);}
 	if (e.altKey||e.metaKey)  ret = "M-" + ret;
 	if (e.ctrlKey) ret = "C-" + ret;
 	callback(ret);
@@ -39,11 +39,9 @@ function onkeydown(e){
 function onkeypress(e){
     var kc=e.keyCode;
     var s=StrCode(e);
-    //if(kc==69){callback("111111");return false}
-    //if(kc==102){callback("FFFFFF");return false}
-    //if(kc==112){callback("PPPPPP");return false}
-    if(kc==13)callback("RET");
-    if(kc==32)callback("SPC");
-    callback(s);
+    switch(kc){
+    case 13:callback("RET");break;
+    case 32:callback("SPC");break;
+    default:callback(s);}
 }
 function onkeyup(e){}
